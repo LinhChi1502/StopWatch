430e1340fd92d9f5c04c797536836b988d5a7b24
LinhChi1502 <linhchidkc@gmail.com>
2020-09-18 20:47:35 +0700

    create file

---
+++ src/StopWatch.java
@@ -0,0 +1,18 @@
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+    public double getElapsedTime() {
+        return (this.endTime - this.startTime) / 1000.0;
+    }
+}
